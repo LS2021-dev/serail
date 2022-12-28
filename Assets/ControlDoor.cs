f01969c095fdac2c8f6d7df0cfb1ab8cc90651de
LS2021-dev <86912875+LS2021-dev@users.noreply.github.com>
2022-12-28 09:20:35 +0100

    Funktionierende Tür

---
+++ Assets/ControlDoor.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDoor : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite openDoor;
+    public Sprite closedDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OpenDoor()
+    {
+        spriteRenderer.sprite = openDoor;
+    }
+
+    void CloseDoor()
+    {
+        spriteRenderer.sprite = closedDoor;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            OpenDoor();
+        }
+    }
+}
